09560b9afeb63589d7269e7e360c118e64174cff
Diptishrivas <Shrivasdipti85@gmail.com>
2026-05-18 01:34:52 +0530

    revison day-1

---
+++ javascipt practice/In-Day-jS-Complete/day_1.js
@@ -0,0 +1,29 @@
+console.log("hello world")
+
+function addition(a,b){
+    return a+b;
+}
+
+console.log(addition(2,3))
+
+for (let i=0; i<5; i++){
+    console.log(i)
+}
+// while loop
+
+// let a = 10;
+
+// while(a<100){
+//     console.log(a)
+//     a++;
+// }
+
+// do while loop
+
+let b = 10;
+do{
+    console.log(b)
+    b++;
+}
+while(b<100)
+
